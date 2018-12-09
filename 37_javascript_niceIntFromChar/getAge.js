421e1ef1bee653d899628a74aaca3ed86eab12ad
Matthias Rudin <matthias.rudin@live.com>
2018-12-09 20:10:42 +0100

    Day 34

---
+++ 37_javascript_niceIntFromChar/getAge.js
@@ -0,0 +1,5 @@
+// origin: https://www.codewars.com/kata/parse-nice-int-from-char-problem/train/javascript
+const getAge = inputString => parseInt(inputString.split("")[0]);
+
+// alternative solution:
+const getAge = parseInt;
