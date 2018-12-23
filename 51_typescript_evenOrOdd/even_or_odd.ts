27b1868d1c1d3379fb36fd3c039ae95ab543d38f
Matthias Rudin <matthias.rudin@live.com>
2018-12-23 15:01:53 +0100

    Day 48

---
+++ 51_typescript_evenOrOdd/even_or_odd.ts
@@ -0,0 +1,2 @@
+// origin: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/typescript
+export const even_or_odd = (n: number): String => (n % 2 === 0 ? 'Even' : 'Odd');
---
+++ 52_typescript_multiplyTwoNumbers/multiply.ts
@@ -0,0 +1,2 @@
+// origin: https://www.codewars.com/kata/523b66342d0c301ae400003b/train/typescript
+export const multiply = (num1: number, num2: number): number => num1 * num2;
