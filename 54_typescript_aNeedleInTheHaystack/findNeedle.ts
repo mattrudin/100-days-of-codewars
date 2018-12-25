fff8ac123b31085d5f0584f5d63e29d2ddc99dd9
Matthias Rudin <matthias.rudin@live.com>
2018-12-25 13:15:04 +0100

    Day 50

---
+++ 54_typescript_aNeedleInTheHaystack/findNeedle.ts
@@ -0,0 +1,2 @@
+// origin : https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/typescript
+export const findNeedle = (haystack: any[]): string => `found the needle at position ${haystack.indexOf('needle')}`;
---
+++ 55_typescript_removeAllExclamationMarksAndAddOneAtTheEnd/remove.ts
@@ -0,0 +1,2 @@
+// origin: https://www.codewars.com/kata/57faf12b21c84b5ba30001b0/train/typescript
+export const remove = (string: string): string => `${string.replace(/!/g, '')}!`;
