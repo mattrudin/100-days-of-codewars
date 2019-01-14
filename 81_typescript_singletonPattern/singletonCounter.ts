a77ca4ec2584530344e638fb7e4f894817cb4280
Matthias Rudin <matthias.rudin@live.com>
2019-01-14 19:48:53 +0100

    Day 70

---
+++ 81_typescript_singletonPattern/singletonCounter.ts
@@ -0,0 +1,27 @@
+// origin: https://www.codewars.com/kata/591445324670baed3200008d/train/typescript
+
+export class SingletonCounter {
+	private static _instance: SingletonCounter = new SingletonCounter();
+	static getInstance() {
+		return this._instance;
+	}
+	private counter: number = 0;
+	public inc(): number {
+		this.counter++;
+		return this.counter;
+	}
+}
+
+// alternative solution:
+export class SingletonCounterAlt {
+	private static _instance: SingletonCounterAlt | undefined;
+	private constructor() {}
+	private _counter = 0;
+	public static getInstance() {
+		if (!this._instance) {
+			this._instance = new SingletonCounterAlt();
+		}
+		return this._instance;
+	}
+	inc = () => ++this._counter;
+}
