66cda4b0ae45d14ad125d47be82a9b952d38912e
Matthias Rudin <matthias.rudin@live.com>
2019-01-22 19:50:35 +0100

    Day 78

---
+++ 90_typescript_oppositeNumber/opposite.ts
@@ -0,0 +1,9 @@
+// origin: https://www.codewars.com/kata/opposite-number/train/typescript
+export class Kata {
+	static opposite(n: number) {
+		return n * -1;
+	}
+}
+
+// alternative solution
+export const KataAlt = { opposite: (n) => -n };
