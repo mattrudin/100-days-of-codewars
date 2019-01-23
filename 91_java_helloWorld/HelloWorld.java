a2f8cd1b341b56601e5fd3f786f2a8017a4b6ace
Matthias Rudin <matthias.rudin@live.com>
2019-01-23 19:28:49 +0100

    Day 79

---
+++ 91_java_helloWorld/HelloWorld.java
@@ -0,0 +1,6 @@
+// origin: https://www.codewars.com/kata/523b4ff7adca849afe000035/solutions/java
+public class HelloWorld {
+    public static String greet() {
+      return "hello world!";
+    }
+  }
