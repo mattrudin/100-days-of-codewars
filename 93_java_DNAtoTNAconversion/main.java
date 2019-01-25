1d2223b029343bf45440580d70813fb2b3268e6e
Matthias Rudin <matthias.rudin@live.com>
2019-01-25 19:02:52 +0100

    Day 81

---
+++ 93_java_DNAtoTNAconversion/main.java
@@ -0,0 +1,6 @@
+// origin: https://www.codewars.com/kata/dna-to-rna-conversion/train/java
+public class Bio {
+    public String dnaToRna(String dna) {
+        return dna.replace('T', 'U');
+    }
+}
