1523d22998d48b38ab4712cda144dfb6c24ebb3c
Matthias Rudin <matthias.rudin@live.com>
2019-01-29 18:48:46 +0100

    Day 85

---
+++ 101_java_BooleanToString/main.java
@@ -0,0 +1,7 @@
+// origin: https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/java
+public class BooleanToString {
+    public static String convert(boolean b) {
+        return b ? "true" : "false";
+    }
+
+}
