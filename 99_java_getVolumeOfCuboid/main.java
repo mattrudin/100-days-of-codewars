292044d473b6829b43e2be5d6e70b54ca5158340
Matthias Rudin <matthias.rudin@live.com>
2019-01-29 18:45:33 +0100

    Day 85

---
+++ 99_java_getVolumeOfCuboid/main.java
@@ -0,0 +1,8 @@
+// origin: https://www.codewars.com/kata/volume-of-a-cuboid/train/java
+public class Kata {
+
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length * width * height;
+    }
+
+}
