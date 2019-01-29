404bf9949a070241fa357a5369814c910feac06c
Matthias Rudin <matthias.rudin@live.com>
2019-01-29 18:47:45 +0100

    Day 85

---
+++ 100_java_SmashWords/main.java
@@ -0,0 +1,10 @@
+
+// origin: https://www.codewars.com/kata/sentence-smash/train/java
+//import java.util.Arrays;
+
+public class SmashWords {
+
+    public static String smash(String... words) {
+        return String.join(" ", words);
+    }
+}
