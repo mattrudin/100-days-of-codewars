0a5b48d8935eabe48082122acdf93053594cc5cf
Matthias Rudin <matthias.rudin@live.com>
2019-01-31 20:24:29 +0100

    Day 87

---
+++ 105_java_endsWith/main.java
@@ -0,0 +1,6 @@
+// origin: https://www.codewars.com/kata/string-ends-with/train/java
+public class Kata {
+    public static boolean solution(String str, String ending) {
+        return str.endsWith(ending);
+    }
+}
